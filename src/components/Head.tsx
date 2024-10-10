6981dc2d9b0ee7b1c37c62ab9c9bede39ae4cf96
Matias <mati.dehoyosmdp@gmail.com>
2024-10-10 17:11:11 +0100

    responsive home

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const Head = () => {
   return (
-    <div className=" md:w-[94%] md:mx-auto md:pt-4 md:border md:border-gray-300 rounded-md bg-gradient-to-r from-white to-gray-300">
-        <div className="hidden md:block w-full">
+    <div className="bg-gray-400 md:bg-transparent">
+        <div className="hidden w-[94%] pt-4 mx-auto md:block rounded-md bg-gradient-to-r from-white to-gray-300">
           <Image src="/bggg.png" 
                  layout="responsive" 
                  alt="Logo shoppyfy"  
@@ -11,13 +11,14 @@ const Head = () => {
                  height={100} 
                  className="w-full md:h-auto object-cover" />
         </div>
-        <div className="w-full md:hidden">
-          <Image src="/bgMovi.png" 
-                 layout="responsive" 
-                 alt="Logo shoppyfy"  
-                 width={400} 
-                 height={300} 
-                 className="w-full h-auto object-cover" />
+        <div className="w-[96%] h-auto mx-auto md:hidden rounded-md bg-gradient-to-r from-white to-gray-300 overflow-hidden">
+          <Image 
+            src="/bgMov.png" 
+            alt="Logo de ShoppOnline"  
+            width={100} 
+            height={100} 
+            className="w-full h-auto object-cover" 
+          />
         </div>
     </div>
   );
