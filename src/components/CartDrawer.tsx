23e1f20b849d62c8a5e53f92d943f5b5f4571168
Matias <mati.dehoyosmdp@gmail.com>
2024-10-12 13:40:29 +0100

    resp cart

--- src/components/CartDrawer.tsx
+++ src/components/CartDrawer.tsx
@@ -22,8 +22,8 @@ const CartDrawer = () => {
 
   return (
     <>
-      {isOpen && <div className="bg-black opacity-30 fixed inset-0 z-40" onClick={toggleCartDrawer}></div>}
-      <div className={`fixed w-[94%] md:w-[600px] h-[calc(100vh-120px)] md:h-[calc(100%-80px)] pb-20 md:pb-60 right-0 md:right-[3%] md:top-40 bg-white shadow-lg z-50 transition-transform duration-1000 ease-in-out ${isOpen ? 'translate-x-0' : 'translate-x-[200%]'} overflow-y-scroll rounded-tl-lg`}>
+      {isOpen && <div className="bg-black opacity-40 fixed inset-0 z-40" onClick={toggleCartDrawer}></div>}
+      <div className={`fixed w-[96%] md:w-[640px] h-[calc(100vh-80px)] md:h-[calc(100%-80px)] pb-20 md:pb-60 mt-2 md:mt-0 right-0 md:right-[3%] md:top-40 bg-white shadow-lg z-50 transition-transform duration-1000 ease-in-out ${isOpen ? 'translate-x-0' : 'translate-x-[200%]'} overflow-y-scroll rounded-tl-lg`}>
         <div className='w-full p-4 mb-4 md:mb-0 flex justify-between items-center'>
           <h2 className="text-xl font-bold text-gray-950">Your cart</h2>
           <button onClick={toggleCartDrawer} className="font-bold text-xl md:text-2xl text-red-400 hover:text-red-500">X</button>
@@ -33,29 +33,32 @@ const CartDrawer = () => {
             <p className="text-center text-gray-500">Your cart is empty</p>
           ) : (
             cart.map((item) => (
-              <div key={item.id} className="w-full flex p-1 md:p-2 justify-between items-center rounded-sm bg-gray-200 border border-gray-200">
+              <div key={item.id} className="w-full flex p-1 md:p-2 justify-between items-center rounded-sm bg-gray-100 border border-gray-200">
+                
                 <Image
                   src={item.thumbnail}
                   alt={item.title}
                   width={80}
                   height={80}
-                  className="w-[50px] h-[50px] md:w-[80px] md:h-[80px] object-contain bg-gray-50 border border-gray-200"
+                  className="w-[80px] h-[80px] object-contain bg-gray-50 border border-gray-200"
                 />
-                <span className='pl-1 md:pl-3 w-32 md:w-40 text-left font-semibold md:font-medium text-md text-gray-800 truncate'>{item.title}</span>
-                <div className='flex items-center mr-2 md:mr-0'>
-                  <button 
-                    onClick={() => handleDecrement(item)} 
-                    disabled={item.quantity === 1} 
-                    className={`bg-white text-sm ${item.quantity === 1 ? 'opacity-50 cursor-not-allowed' : 'opacity-100'} text-gray-900 px-1 md:px-2 rounded border border-gray-300 md:hover:scale-110`}>
-                    &ndash;
-                  </button> 
-                  <span className='mx-1 md:mx-2 text-sm text-gray-800'>{item.quantity}</span>
-                  <button 
-                    onClick={() => handleIncrement(item)} 
-                    className='text-sm bg-white text-gray-900 px-1 md:px-2 rounded border border-gray-300 md:hover:scale-110'>
-                    +
-                  </button> 
-                </div> 
+                <div className='flex flex-col md:flex-row w-40 md:w-80 md:justify-between pr-2 md:pr-4 gap-2'>
+                    <span className='pl-1 md:pl-2 text-left font-semibold md:font-medium text-md text-gray-800 truncate'>{item.title}</span>
+                    <div className='flex items-center'>
+                      <button 
+                        onClick={() => handleDecrement(item)} 
+                        disabled={item.quantity === 1} 
+                        className={`bg-white text-sm ${item.quantity === 1 ? 'opacity-50 cursor-not-allowed' : 'opacity-100'} text-gray-900 px-2 rounded border border-gray-300 md:hover:scale-110`}>
+                        &ndash;
+                      </button> 
+                      <span className='mx-2 text-md text-gray-800'>{item.quantity}</span>
+                      <button 
+                        onClick={() => handleIncrement(item)} 
+                        className='text-sm bg-white text-gray-900 px-2 rounded border border-gray-300 md:hover:scale-110'>
+                        +
+                      </button> 
+                    </div> 
+                </div>
                 <span className='md:text-md font-bold text-gray-800 mr-2 md:mr-0'>${(item.price * item.quantity).toFixed(2)}</span>
                 <button onClick={() => removeFromCart(item.id)} className='text-red-500 md:text-2xl md:mr-4 md:hover:scale-110'><BiTrash /></button>
               </div>
